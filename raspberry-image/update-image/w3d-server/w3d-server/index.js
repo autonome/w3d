479b006607d43c158a9219cd54510769efbc9da3
dietrich ayala <autonome@gmail.com>
2016-06-17 13:18:45 +0100

    Add websocket server

--- raspberry-image/update-image/w3d-server/w3d-server/index.js
+++ raspberry-image/update-image/w3d-server/w3d-server/index.js
@@ -1,14 +1,35 @@
 /* globals require */
 "use strict";
 const http = require("http");
+const httpPort = 8000;
+const ws = require("ws");
+const wsPort = 8009;
+
 
 (() => {
+
+  // Web server
   console.log(`starting w3d-server on port 80`);
   const server = http.createServer((req, res) => {
     res.end("pong");
   });
-  server.listen(80);
+  server.listen(httpPort);
 
+  // Web socket server
+  var wss = new ws.Server({ port: wsPort });
+  wss.on('connection', function(conn) {
+
+    conn.on('message', function(msg) {
+
+      if (msg.blink) {
+
+        // Make stuff blink.
+
+      }
+
+    });
+
+  });
 
   const shutdown = () => {
     console.log("shutting down...");
